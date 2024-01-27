c23c4a8530224dfdc6163b244c6aef6b4cf6a7f1
C.S. Melis <cees3331x00@gmail.com>
2024-01-27 16:25:18 +0100

    Document Common.h parameter names

--- include/Common.h
+++ include/Common.h
@@ -11,14 +11,14 @@ public:
     INI_Reader();
     ~INI_Reader();
 
-    bool open(LPCSTR, bool = false);
+    bool open(LPCSTR path, bool throwExceptionOnFail = false);
     bool read_header();
-    bool is_header(LPCSTR);
+    bool is_header(LPCSTR name);
     bool read_value();
-    bool is_value(LPCSTR);
-    int get_value_int(UINT = 0);
-    float get_value_float(UINT = 0);
-    LPCSTR get_value_string(UINT = 0);
+    bool is_value(LPCSTR name);
+    int get_value_int(UINT index = 0);
+    float get_value_float(UINT index = 0);
+    LPCSTR get_value_string(UINT index = 0);
     void close();
 
 private:
